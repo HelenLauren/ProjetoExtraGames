b741eb0308efaa887d0353637f21740cae39b3d3
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-05-28 10:14:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form-user');
+const nome-input = document.getElementById('');
+const nome-input = document.getElementById('');
+const nome-input = document.getElementById('').querySelector('t-body')
