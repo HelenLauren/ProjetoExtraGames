ad4d54a51d0546204c1a5b100c0bb67d7e0cfc02
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-05-29 09:07:44 -0300

    script pronto?

--- script.js
+++ script.js
@@ -1,4 +1,61 @@
 const form = document.getElementById('form-user');
-const nome-input = document.getElementById('');
-const nome-input = document.getElementById('');
-const nome-input = document.getElementById('').querySelector('t-body')
+const nomeInput = document.getElementById('nome');
+const emailInput = document.getElementById('email');
+const tabela = document.getElementById('tabela-usuarios').querySelector('tbody');
+
+let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+let editIndex = null;
+
+function salvarDados() {
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+function renderizarTabela() {
+  tabela.innerHTML = '';
+  usuarios.forEach(user, index) => {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = `
+      <td>${user.nome}</td>
+      <td>${user.nome}</td>
+      <td>
+          <button onclick="editarUsuario(${index})">Editar</button>
+          <button onclick="deletarUsuario(${index})">Excluir</button>
+      </td>
+    `;
+
+     tabela.appendChild(tr);
+   });
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nome = nomeInput.value;
+  const email = emailInput.value;
+
+  if (editIndex === null) {
+    usuarios.push({nome, email});
+  } else {
+    usuarios[editIndex] = {nome, email};
+    editIndex = null;
+  }
+
+  salvarDados();
+  renderizarTabela();
+  form.reset();
+});
+
+function editarUsuario(index) {
+  const user = usuarios[index];
+  nomeInput.value = user.nome;
+  emailInput.value = user.email;
+  editIndex = index;
+}
+
+function deletarUsuario(index) {
+  usuarios.splice(index, 1);
+  salvarDados();
+  renderizarTabela();
+}
+
+renderizarTabela();
