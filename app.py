7a0131c3a3e652b43374c54b6e1aa2f68e0de57f
Helen Lauren Bonato <167373993+HelenLauren@users.noreply.github.com>
2025-05-29 09:22:37 -0300

    instrucoes instalação back

---
+++ app.py
@@ -0,0 +1,5 @@
+#instrucoes para instalação
+#python -m venv venv
+#venv\Scripts\activate
+#pip install flask flask-cors mysql-connector-python
+#talvez precise instalar = pip install flask flask-cors mysql-connector-python python-dotenv
